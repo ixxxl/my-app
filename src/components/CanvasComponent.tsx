dd90aa367759eb8f58b4d9aac2d781598bed12b0
ixxxl <106831329+ixxxl@users.noreply.github.com>
2023-03-02 13:02:40 +0200

    11

--- src/components/CanvasComponent.tsx
+++ src/components/CanvasComponent.tsx
@@ -1,22 +1,52 @@
 import React, { useEffect, useRef } from 'react';
-const mockData: number[] = [300, 400, 500, 150];
+import { useAxiosGet } from '../services/hooks/axios';
+// const mockData: number[] = [300, 400, 500, 150];
+interface IProps {
+  idno: number | undefined;
+}
 
-const Canvas = () => {
+
+
+
+
+
+const Canvas = (props: IProps) => {
   const canvasRef = useRef(null);
+
+  const { data, error, loaded } = useAxiosGet(
+    `http://localhost:3010/statistic?idno=${props.idno}`
+  );
+  let a: number[];
+  let max: number;
+  useEffect(() => {
+    if (data) {
+      a = data[0].semesters;
+      // for(let x of a){
+      //   console.log(x)
+
+      // }
+      console.log(a.length);
+      max = Math.max(...a);
+      console.log(200 / max);
+      console.log(a[0]);
+      // console.log(Math.min(...a))
+    }
+  }, [data]);
 
   const draw = (ctx: any) => {
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.lineWidth = '5';
-    ctx.moveTo(mockData[0], mockData[1]);
-    ctx.lineTo(mockData[2], mockData[3]);
+    // ctx.moveTo(50, a);
+    // ctx.lineTo(500, a);
+    ctx.fillRect(60, 100, 100, 40);
     ctx.stroke();
-   // ctx.arc(50, 100, 20, 0, 2 * Math.PI);
-    ctx.fill();
+    // ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+   // ctx.fill();
   };
   useEffect(() => {
     const canvas: any = canvasRef.current;
-    canvas.width = 600;
+    canvas.width = 200;
     canvas.height = 400;
     const context: any = canvas.getContext('2d');
 
@@ -40,3 +70,27 @@ export default Canvas;
 //     ctx.stroke();
 //     ctx.fill();
 //   };
+
+
+
+
+
+// const SimpleCanvasExample: React.FC<{}> = () => {
+//   let canvasRef = useRef<HTMLCanvasElement | null>(null);
+//   let canvasCtxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+//   useEffect(() => {
+//     // Initialize
+//     if (canvasRef.current) {
+//       canvasCtxRef.current = canvasRef.current.getContext('2d');
+//       let ctx = canvasCtxRef.current;
+//       ctx!.beginPath();
+//       ctx!.arc(95, 50, 40, 0, 2 * Math.PI);
+//       ctx!.stroke();
+//     }
+//   }, []);
+
+//   return <canvas ref={canvasRef}></canvas>;
+// };
+
+// export  SimpleCanvasExample;
